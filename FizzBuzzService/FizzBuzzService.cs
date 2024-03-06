ac5a6900509b2e74d141e1548b6feae85b984ab3
karolis <karolis.pakalnis@simplanova.com>
2024-03-06 15:14:57 +0200

    implementation for division by 5

--- FizzBuzzService/FizzBuzzService.cs
+++ FizzBuzzService/FizzBuzzService.cs
@@ -5,12 +5,18 @@ public class FizzBuzzService
     public static string CheckIfIntegerDivisibleByThreeOrFiveAndReturnResult(int integer)
     {
         var numberIsDivisibleByThree = integer % 3 == 0;
+        var numberIsDivisibleByFive = integer % 5 == 0;
+
         var result = string.Empty;
 
         if (numberIsDivisibleByThree)
         {
             //TODO: add enum or constants.
             result = "Fizz";
+        }
+        else if (numberIsDivisibleByFive)
+        {
+            result = "Buzz";
         }
 
         return result;
